07011722ada30afd176de42246a4d97ad55cf8d1
Lionel Flandrin <lionel.flandrin@gmail.com>
2015-11-30 23:58:04 +0100

    Use "image" library to handle compressed formats

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use std::fs::{File, OpenOptions};
-use std::io::{Read, Write};
+extern crate image;
 
-const XRES: usize = 800;
-const YRES: usize = 1200;
+use std::path::Path;
 
 fn main() {
     let argv: Vec<_> = std::env::args().collect();
@@ -12,61 +10,90 @@ fn main() {
         return;
     }
 
-    let mut buffer = Box::new([[(0, 0, 0); XRES]; YRES]);
+    let in_file = &Path::new(&argv[1]);
+    let out_file = &Path::new(&argv[2]);
 
-    let mut in_file = File::open(&argv[1]).unwrap();
-    let mut out_file =
-        OpenOptions::new()
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .open(&argv[2]).unwrap();
+    let image = image::open(in_file).unwrap();
 
-    for y in 0..YRES {
-        for x in 0..XRES {
-            let mut b = [0; 3];
+    let image = image.to_rgb();
+    let width = image.width() as usize;
+    let height = image.height() as usize;
+    let mut image = image.into_raw();
 
-            if in_file.read(&mut b).unwrap() != b.len() {
-                panic!("Short read");
-            }
+    // Convert to an array of one tuple (u8, u8, u8) per pixel
+    let pixels = unsafe {
+        let raw = image.as_mut_ptr() as *mut (u8, u8, u8);
 
-            buffer[y][x] = (b[0], b[1], b[2]);
-        }
-    }
+        std::slice::from_raw_parts_mut(raw, width * height)
+    };
 
-    for y in 0..YRES {
-        let line: Vec<_> = 
-            buffer[y].iter().filter_map(|&c| {
-                let (r, g, b) = c;
+    glitch(pixels, width, height);
 
-                let r = r as u32;
-                let g = g as u32;
-                let b = b as u32;
+    image::save_buffer(out_file,
+                       &image,
+                       width as u32,
+                       height as u32,
+                       image::ColorType::RGB(8)).unwrap();
 
-                if r + g + b > 130 {
-                    Some (c)
-                } else {
-                    None
-                }
-            }).collect();
+    // for y in 0..YRES {
+    //     for x in 0..XRES {
+    //         let mut b = [0; 3];
 
-        let start = (XRES - line.len()) / 2;
+    //         if in_file.read(&mut b).unwrap() != b.len() {
+    //             panic!("Short read");
+    //         }
 
-        buffer[y].sort_by(|&a, &b| {
+    //         buffer[y][x] = (b[0], b[1], b[2]);
+    //     }
+    // }
+
+    // for y in 0..YRES {
+    //     // let line: Vec<_> =
+    //     //     buffer[y].iter().filter_map(|&c| {
+    //     //         let (r, g, b) = c;
+
+    //     //         let r = r as u32;
+    //     //         let g = g as u32;
+    //     //         let b = b as u32;
+
+    //     //         if r + g + b > 130 {
+    //     //             Some (c)
+    //     //         } else {
+    //     //             None
+    //     //         }
+    //     //     }).collect();
+
+    //     // let start = (XRES - line.len()) / 2;
+
+    //     buffer[y].sort_by(|&a, &b| {
+    //         a.0.cmp(&b.0)
+    //     });
+
+    //     // for i in 0..line.len() {
+    //     //     buffer[y][start + i] = line[i];
+    //     // }
+    // }
+
+    // for y in 0..YRES {
+    //     for x in 0..XRES {
+    //         let col = buffer[y][x];
+    //         let b = [col.0, col.1, col.2];
+
+    //         out_file.write(&b).unwrap();
+    //     }
+    // }
+}
+
+fn glitch(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
+    
+    for y in 0..height {
+        let offset = y * width;
+
+        let line = &mut pixels[offset..offset + width];
+
+        line.sort_by(|&a, &b| {
             a.0.cmp(&b.0)
         });
-
-        for i in 0..line.len() {
-            buffer[y][start + i] = line[i];
-        }
     }
 
-    for y in 0..YRES {
-        for x in 0..XRES {
-            let col = buffer[y][x];
-            let b = [col.0, col.1, col.2];
-
-            out_file.write(&b).unwrap();
-        }
-    }
 }
