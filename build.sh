9afbbd10965a3de53409eb5344a3e93b64d2974d
Lionel Flandrin <lionel.flandrin@gmail.com>
2015-12-01 18:29:24 +0100

    Added high frequency detection glitch

--- build.sh
+++
@@ -1,4 +0,0 @@
-
-out="assets/out_$(date '+%Y-%m-%d_%H:%M:%S').png"
-
-cargo run --release assets/image.rgb assets/out.rgb && convert -depth 8 -size 800x1200 assets/out.rgb "$out" && qiv "$out"
--- src/main.rs
+++ src/main.rs
@@ -27,65 +27,17 @@ fn main() {
         std::slice::from_raw_parts_mut(raw, width * height)
     };
 
-    glitch_block_sin(pixels, width, height);
+    glitch_hf(pixels, width, height);
 
     image::save_buffer(out_file,
                        &image,
                        width as u32,
                        height as u32,
                        image::ColorType::RGB(8)).unwrap();
-
-    // for y in 0..YRES {
-    //     for x in 0..XRES {
-    //         let mut b = [0; 3];
-
-    //         if in_file.read(&mut b).unwrap() != b.len() {
-    //             panic!("Short read");
-    //         }
-
-    //         buffer[y][x] = (b[0], b[1], b[2]);
-    //     }
-    // }
-
-    // for y in 0..YRES {
-    //     // let line: Vec<_> =
-    //     //     buffer[y].iter().filter_map(|&c| {
-    //     //         let (r, g, b) = c;
-
-    //     //         let r = r as u32;
-    //     //         let g = g as u32;
-    //     //         let b = b as u32;
-
-    //     //         if r + g + b > 130 {
-    //     //             Some (c)
-    //     //         } else {
-    //     //             None
-    //     //         }
-    //     //     }).collect();
-
-    //     // let start = (XRES - line.len()) / 2;
-
-    //     buffer[y].sort_by(|&a, &b| {
-    //         a.0.cmp(&b.0)
-    //     });
-
-    //     // for i in 0..line.len() {
-    //     //     buffer[y][start + i] = line[i];
-    //     // }
-    // }
-
-    // for y in 0..YRES {
-    //     for x in 0..XRES {
-    //         let col = buffer[y][x];
-    //         let b = [col.0, col.1, col.2];
-
-    //         out_file.write(&b).unwrap();
-    //     }
-    // }
 }
 
 fn glitch_sort(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
-    
+
     for y in 0..height {
         let offset = y * width;
 
@@ -99,7 +51,7 @@ fn glitch_sort(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
 }
 
 fn glitch_filter(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
-    
+
     for y in 0..height {
         let offset = y * width;
 
@@ -137,7 +89,7 @@ fn glitch_filter(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
 }
 
 fn glitch_filter_sort(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
-    
+
     for y in 0..height {
         let offset = y * width;
 
@@ -175,7 +127,7 @@ fn glitch_filter_sort(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
 }
 
 fn glitch_detour(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
-    
+
     for y in 0..height {
         let offset = y * width;
 
@@ -219,7 +171,7 @@ fn glitch_detour(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
 }
 
 fn glitch_block(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
-    
+
     for y in 0..height {
         let offset = y * width;
 
@@ -254,7 +206,7 @@ fn glitch_block(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
         let detour = &mut line[start..end];
 
         for i in 0..detour.len() {
-            detour[i] = detour[i & !0x3f]
+            detour[i] = detour[i & !0xff]
         }
 
         // detour.sort_by(|&a, &b| {
@@ -267,7 +219,7 @@ fn glitch_block(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
 }
 
 fn glitch_block_sin(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
-    
+
     for y in 0..height {
         let offset = y * width;
 
@@ -319,8 +271,51 @@ fn glitch_block_sin(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
     }
 }
 
+fn glitch_hf(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
+
+    for y in 0..height {
+        let offset = y * width;
+
+        let diff = 100;
+
+        let line = &mut pixels[offset..offset + width];
+
+        let mut out = vec![(0u8, 0u8, 0u8); width];
+
+        let edge = 100;
+
+        for i in diff..(line.len() - diff) {
+            let a = col_sum(line[i - diff]) as i32;
+            let b = col_sum(line[i]) as i32;
+            let c = col_sum(line[i + diff]) as i32;
+
+            if (a - b).abs() > edge && (b - c).abs() > edge {
+                out[i] = line[i]
+            } else {
+                out[i] = line[i & !0xff]
+            }
+        }
+
+        for i in 0..line.len() {
+            line[i] = out[i];
+        }
+    }
+}
+
 fn col_sum(c: (u8, u8, u8)) -> u16 {
     let (r, g, b) = c;
 
     r as u16 + g as u16 + b as u16
+}
+
+fn complement(c: (u8, u8, u8)) -> (u8, u8, u8) {
+    let (r, g, b) = c;
+
+    (!r, !g, !b)
+}
+
+fn rotate(c: (u8, u8, u8)) -> (u8, u8, u8) {
+    let (r, g, b) = c;
+
+    (g, b, r)
 }
