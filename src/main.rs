cf6f43bd87a8269a2a40c943c7566528ad4fcd0d
Lionel Flandrin <lionel.flandrin@gmail.com>
2015-12-01 01:20:06 +0100

    Added a bunch of glitches for "glitch me please"

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ fn main() {
         std::slice::from_raw_parts_mut(raw, width * height)
     };
 
-    glitch(pixels, width, height);
+    glitch_block_sin(pixels, width, height);
 
     image::save_buffer(out_file,
                        &image,
@@ -84,7 +84,7 @@ fn main() {
     // }
 }
 
-fn glitch(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
+fn glitch_sort(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
     
     for y in 0..height {
         let offset = y * width;
@@ -96,4 +96,231 @@ fn glitch(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
         });
     }
 
+}
+
+fn glitch_filter(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
+    
+    for y in 0..height {
+        let offset = y * width;
+
+        let line = &mut pixels[offset..offset + width];
+
+        let filtered: Vec<_> = line.iter().filter_map(|&c| {
+            let (r, g, b) = c;
+
+            let r = r as u32;
+            let g = g as u32;
+            let b = b as u32;
+
+            if g < r && g < b { //if r + g + b > 130 {
+                Some (c)
+            } else {
+                None
+            }
+        }).collect();
+
+        // line.sort_by(|&a, &b| {
+        //     a.0.cmp(&b.0)
+        // });
+
+        for i in 0..line.len() {
+            line[i] = (0xff, 0xff, 0xff);
+        }
+
+        let start = (line.len() - filtered.len()) / 2;
+
+        for i in 0..filtered.len() {
+            line[start + i] = filtered[i];
+        }
+    }
+
+}
+
+fn glitch_filter_sort(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
+    
+    for y in 0..height {
+        let offset = y * width;
+
+        let line = &mut pixels[offset..offset + width];
+
+        let mut filtered: Vec<_> = line.iter().filter_map(|&c| {
+            let (r, g, b) = c;
+
+            let r = r as u32;
+            let g = g as u32;
+            let b = b as u32;
+
+            if g < r && g < b { //if r + g + b > 130 {
+                Some (c)
+            } else {
+                None
+            }
+        }).collect();
+
+        filtered.sort_by(|&a, &b| {
+            a.0.cmp(&b.0)
+        });
+
+        for i in 0..line.len() {
+            line[i] = (0xff, 0xff, 0xff);
+        }
+
+        let start = (line.len() - filtered.len()) / 2;
+
+        for i in 0..filtered.len() {
+            line[start + i] = filtered[i];
+        }
+    }
+
+}
+
+fn glitch_detour(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
+    
+    for y in 0..height {
+        let offset = y * width;
+
+        let line = &mut pixels[offset..offset + width];
+
+        let mut start = 0;
+
+        for i in 0..line.len() {
+            let (r, g, b) = line[i];
+
+            if r < 245 || g < 245 || b < 245 {
+                start = i;
+                break;
+            }
+        }
+
+        let mut end = line.len();
+
+        for i in (0..line.len()).rev() {
+            let (r, g, b) = line[i];
+
+            if r < 245 || g < 245 || b < 245 {
+                end = i;
+                break;
+            }
+        }
+
+        if start >= end {
+            continue;
+        }
+
+        let detour = &mut line[start..end];
+
+        detour.sort_by(|&a, &b| {
+            let a = col_sum(a);
+            let b = col_sum(b);
+            a.cmp(&b)
+        });
+
+    }
+}
+
+fn glitch_block(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
+    
+    for y in 0..height {
+        let offset = y * width;
+
+        let line = &mut pixels[offset..offset + width];
+
+        let mut start = 0;
+
+        for i in 0..line.len() {
+            let (r, g, b) = line[i];
+
+            if r < 245 || g < 245 || b < 245 {
+                start = i;
+                break;
+            }
+        }
+
+        let mut end = line.len();
+
+        for i in (0..line.len()).rev() {
+            let (r, g, b) = line[i];
+
+            if r < 245 || g < 245 || b < 245 {
+                end = i;
+                break;
+            }
+        }
+
+        if start >= end {
+            continue;
+        }
+
+        let detour = &mut line[start..end];
+
+        for i in 0..detour.len() {
+            detour[i] = detour[i & !0x3f]
+        }
+
+        // detour.sort_by(|&a, &b| {
+        //     let a = col_sum(a);
+        //     let b = col_sum(b);
+        //     a.cmp(&b)
+        // });
+
+    }
+}
+
+fn glitch_block_sin(pixels: &mut[(u8, u8, u8)], width: usize, height: usize) {
+    
+    for y in 0..height {
+        let offset = y * width;
+
+        let line = &mut pixels[offset..offset + width];
+
+        let mut start = 0;
+
+        for i in 0..line.len() {
+            let (r, g, b) = line[i];
+
+            if r < 245 || g < 245 || b < 245 {
+                start = i;
+                break;
+            }
+        }
+
+        let mut end = line.len();
+
+        for i in (0..line.len()).rev() {
+            let (r, g, b) = line[i];
+
+            if r < 245 || g < 245 || b < 245 {
+                end = i;
+                break;
+            }
+        }
+
+        if start >= end {
+            continue;
+        }
+
+        let detour = &mut line[start..end];
+
+        let align = ((y as f32).sin() * 256.).abs() as usize + 1;
+
+        for i in 0..detour.len() {
+
+            let p = i - (i % align);
+
+            detour[i] = detour[p]
+        }
+
+        // detour.sort_by(|&a, &b| {
+        //     let a = col_sum(a);
+        //     let b = col_sum(b);
+        //     a.cmp(&b)
+        // });
+
+    }
+}
+
+fn col_sum(c: (u8, u8, u8)) -> u16 {
+    let (r, g, b) = c;
+
+    r as u16 + g as u16 + b as u16
 }
